7dcae4fb5a41feab1a5e221a92fb1c196e0062bb
Fabriciocod <fva.mcz@gmail.com>
2023-08-31 16:38:03 -0300

    alterações desfeitas

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,32 @@
 import './globals.css'
-
-
-
+import styles from './styles.module.css'
 
 export const metadata = {
   title: 'Trave Landing Page',
   
 }
 
+function Logo(){
+  return(
+    <p className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
+  );
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body> {children}</body>
+      <body> 
+        
+        <header>
+          <Logo/>
+        </header>
+        
+        
+        <main>
+          {children}
+        </main>
+        
+        </body>
     </html>
   )
 }
