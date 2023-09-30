beb8c9d6987c00f37227ba3e1093b65ee82fb3b4
Fabriciocod <fva.mcz@gmail.com>
2023-09-30 11:25:00 -0300

    Adicionado Menu

--- app/layout.js
+++ app/layout.js
@@ -6,9 +6,25 @@ export const metadata = {
   
 }
 
-function Logo(){
+function Logo({x,y}){
   return(
-    <p className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
+    <p style={{left: x, top: y}}className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
+  );
+}
+
+function ItemMenu({nome}){
+  return(
+    <p className={styles.itemMenu}>{nome}</p>
+  );
+}
+/*definir o menu do site*/
+
+function Menu({children, x, y}){
+  return(
+    <ul style={{left: x, top: y}}className={styles.menu}>
+      {children.map((itemMenu =>
+        <li key={itemMenu.nome} > {itemMenu}</li>))}
+    </ul>
   );
 }
 
@@ -18,7 +34,18 @@ export default function RootLayout({ children }) {
       <body> 
         
         <header>
-          <Logo/>
+          <Logo x = {135} y = {53}/>
+          
+          <Menu x = {435} y = {60}>
+          
+            <ItemMenu  nome = "Home"/>
+            <ItemMenu  nome = "About"/>
+            <ItemMenu  nome = "Contact"/>
+            <ItemMenu  nome = "Blog"/>
+            <ItemMenu  nome = "Vídeo"/>
+          
+          </Menu>
+
         </header>
         
         
