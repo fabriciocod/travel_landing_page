40a6715b22184002b6a3fa843fd22320c3b39967
Fabriciocod <fva.mcz@gmail.com>
2023-10-04 15:43:32 -0300

    Atividade Concluida do Titulo, Texto e contato

--- app/layout.js
+++ app/layout.js
@@ -1,87 +1,97 @@
 import './globals.css'
 import styles from './styles.module.css'
+// import { Quicksand } from 'next/font/google'
+import Image from 'next/image';
+
 
 export const metadata = {
-  title: 'Trave Landing Page',
-  
+  title: 'Travel Landing Page',
 }
 
-function Logo({x,y}){
-  return(
-    <p style={{left: x, top: y}}className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
+// const quicksand = Quicksand({
+//   subsets: ['latin'],
+//   display: 'swap',
+// })
+
+function Logo({x, y}){
+  return (
+    <p style={{left: x, top: y }} className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
   );
 }
 
 function ItemMenu({nome}){
-  return(
+  return (
     <p className={styles.itemMenu}>{nome}</p>
   );
 }
-/*definir o menu do site*/
 
 function Menu({children, x, y}){
   return(
-    <ul style={{left: x, top: y}}className={styles.menu}>
-      {children.map((itemMenu =>
-        <li key={itemMenu.nome} > {itemMenu}</li>))}
+    <ul style={{left: x, top: y}} className={styles.menu}>
+      {children.map((itemMenu) => (
+        <li key={itemMenu.nome}>{itemMenu}</li>
+      ))}
     </ul>
   );
 }
+//aaaa
 
-function Contato({telefone}){
-  return(
-    <div className={styles.contato}>
-      <img src='phone_icon.png' alt='Phone'/>
-      <span>{telefone}</span>
-    </div>
+//aaaa
+function Titulo({x, y}){
+  return (
+    <h1 style={{left: x, top: y }} className={styles.titulo}>Your perfect honeymoon</h1>
   );
 }
-
-
-function Titulo({x,y}){
-  return(
-    <h1 style={{left: x, top: y}}className={styles.titulo}>Your perfect honeymoon</h1>
+function Texto({x, y}){
+  return (
+    <h2 style={{left: x, top: y }} className={styles.texto}>Mayami is more than a honeymoon destination – it’s a perfect romantic spot for couples who like  spending time in beach. </h2>
   );
 }
+//aaaaa
+function Icon({x, y}){
+  return (
+    <p style={{left: x, top: y }} className={styles.icon}><Image src='/phone_icon.png'width={19} height={19}/></p>
+  );
+  
+}
 
-function Texto({x,y}){
-  return(
-    <h2 style={{left: x, top: y}}className={styles.texto}>Mayami is more than a honeymoon destination – it's a perfect romantic spot for couples who like  spending time in beach.</h2>
+//aaaaa
+function Contato({x, y}){
+  return (
+    <p style={{left: x, top: y }} className={styles.contato}>+38(097)8849989</p>
   );
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body> 
-        
-        <header className={styles.cabecalho}>
-          <Logo x = {135} y = {53}/>
-          
-          <Menu x = {435} y = {60}>
-          
-            <ItemMenu  nome = "Home"/>
-            <ItemMenu  nome = "About"/>
-            <ItemMenu  nome = "Contact"/>
-            <ItemMenu  nome = "Blog"/>
-            <ItemMenu  nome = "Vídeo"/>
-                      
-          </Menu>
-         
-         <Contato className={styles.contato} telefone={'+38(097)8849989'}/>
+      <body  className={styles.corpo}>
+        <header>
+          <Logo x={135} y={53}/>
+          <Menu x={435} y={60}>
+            
+            <ItemMenu nome="Home"/>
+            <ItemMenu nome="About"/>
+            <ItemMenu nome="Contact"/>
+            <ItemMenu nome="Blog"/>
+            <ItemMenu nome="Videos"/>
 
-         
+          </Menu>
+
+          <Icon></Icon>
+          <Contato></Contato>
+
         </header>
         
-        
         <main>
-          <Titulo x = {135} y = {238}></Titulo>
-
-          <Texto x = {135} y = {465.4}></Texto>
+        
+          <Titulo></Titulo>
+          <Texto></Texto>
           {children}
+          
         </main>
         
-        </body>
+      </body>
     </html>
   )
 }
