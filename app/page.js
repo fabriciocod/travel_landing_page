446cca94a989c9968be13e53e5296d87002570ae
Fabriciocod <fva.mcz@gmail.com>
2023-10-19 14:12:20 -0300

    Adicionando botão com stilo e posição.

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+function Botao({x,y}){
+  return(
+    <button style={{left: x, top: y}}className={styles.botao}>Start</button>
+  )
+}
 export default function Home() {
   return (
-      <h1></h1>
+      <Botao x={135} y={601.72}/>
   )
 }
