cd935433728ef50d319b8c2c95ef9fc77ee1739c
Fabriciocod <fva.mcz@gmail.com>
2023-10-21 09:17:57 -0300

    Posicionamento dos Cards

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import styles from './page.module.css'
 import Image from 'next/image'
 import hawaii from '../public/hawaii.png'
 import seta from '../public/arrow-right.svg'
-import cyrus from '../public/cyrus.png'
+import cyprus from '../public/cyprus.png'
 import bali from '../public/bali.png'
 
 
@@ -10,6 +10,14 @@ import bali from '../public/bali.png'
 function Botao({x,y}){
   return(
     <button style={{left: x, top: y}}className={styles.botao}>Start</button>
+  )
+}
+
+function Cards({x,y, children}){
+  return(
+    <div style={{left: x, top: y}}className={styles.cards}>
+      {children}
+    </div>
   )
 }
 
@@ -25,11 +33,13 @@ function Card({localidade, imagem,}){
 export default function Home() {
   return (
     <>
-    <Botao x={135} y={601.72}/>
-    <Card localidade="Hawaii" imagem={hawaii}/>
-    <Card localidade="Cyrus" imagem={cyrus}/>
-    <Card localidade="Bali" imagem={bali}/>
+      <Botao x={135} y={601.72}/>
 
+      <Cards x={135} y={801}>
+        <Card localidade="Hawaii" imagem={hawaii}/>
+        <Card localidade="Cyprus" imagem={cyprus}/>
+        <Card localidade="Bali" imagem={bali}/>
+      </Cards>
     </>
 
     
